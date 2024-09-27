963660fbd32ab32cb752d73ec55fc70169ddc029
Andrei <andrebest94@gmail.com>
2024-09-27 10:55:49 +0200

    first commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,8 +9,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class App 
 {
     public static void main( String[] args )
-    {   System.setProperty("webdriver.chrome.driver","C:\\Users\\a.adam\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+    {   //chrome browser config
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\a.adam\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        //per firefox  System.setProperty("webdriver.gecko.driver","C:\\Users\\a.adam\\Downloads\\chromedriver-win64\\chromedriver-win64\\geckodriver.exe");
+        //!!bisogna scaricare il driver di firefox della versione che si ha dal sito ufficiale selenium
+        //FirefoxDriver driver= new FirefoxDriver();
 
+        //per Edge bisogna fare la stessa cosa
+        //System.setProperty("webdriver.edge.driver","C:\\Users\\a.adam\\Downloads\\chromedriver-win64\\chromedriver-win64\\msedgedriver.exe");
+        // EdgeDriver driver= new EdgeDriver();
         System.out.println( "Hello World!" );
         ChromeDriver driver= new ChromeDriver();
         driver.get("http://www.google.com");

ce826bbd013ef89525ba6e29a81ce33e73fd7737
Andrei <andrebest94@gmail.com>
2024-09-27 12:32:19 +0200

    first commit

--- src/main/java/ui/test/LoginTest.java
+++ src/main/java/ui/test/LoginTest.java
@@ -13,8 +13,12 @@ public class LoginTest {
         driver.get("https://www.saucedemo.com");
         //identified by id name of xpath , we have a method ,where we insert the username standard_user
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
-
-
+        //same thing also for the password, as the user name
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        //third step is to identify the id button , but with the copy of xpath
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+        //the last step is to close the browser
+        driver.close();
 
 
     }

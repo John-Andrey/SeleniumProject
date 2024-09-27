c7b0abccff6485d41fcd33808330e50f661c0aa8
Andrei <andrebest94@gmail.com>
2024-09-27 14:50:09 +0200

    first commit

--- src/main/java/ui/test/LoginTest.java
+++ src/main/java/ui/test/LoginTest.java
@@ -2,14 +2,32 @@ package ui.test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class LoginTest {
+    public static String browser="chrome";
+    //driver = global
+    public static WebDriver driver;
     public static void main( String[] args ){
 
-        WebDriverManager.chromedriver().setup();
-        ChromeDriver driver = new ChromeDriver();
+        if(browser.equals("Firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+        else if(browser.equals("chrome")){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+        else if(browser.equals("edge")){
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        }
+
+
         driver.get("https://www.saucedemo.com");
         //identified by id name of xpath , we have a method ,where we insert the username standard_user
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
@@ -19,6 +37,9 @@ public class LoginTest {
         driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
         //the last step is to close the browser
         driver.close();
+
+
+
 
 
     }
